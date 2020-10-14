24ac5379eae09f4e1f410f61b2ba4aa02aacd97c
Ndofire <siyandasokhela@gmail.com>
2020-10-14 12:10:07 +0200

    Updates

---
+++ precache-manifest.75ec92112c9a2291fd1735d07803d4c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ff0d23c5d4a5cc550e2dc875dff2a3",
+    "url": "/The-Look-Up/index.html"
+  },
+  {
+    "revision": "02834973237d93adab01",
+    "url": "/The-Look-Up/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "33c4b1103691dd172c3b",
+    "url": "/The-Look-Up/static/css/main.6cf8261e.chunk.css"
+  },
+  {
+    "revision": "02834973237d93adab01",
+    "url": "/The-Look-Up/static/js/2.0471ef47.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/The-Look-Up/static/js/2.0471ef47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c4b1103691dd172c3b",
+    "url": "/The-Look-Up/static/js/main.50c25b51.chunk.js"
+  },
+  {
+    "revision": "51d2109cd91fdaac5e2a",
+    "url": "/The-Look-Up/static/js/runtime-main.b247f5ad.js"
+  },
+  {
+    "revision": "91feba1fee54b213ed5ce8b4ddce9878",
+    "url": "/The-Look-Up/static/media/img_.91feba1f.jpg"
+  },
+  {
+    "revision": "393751aa3eb58cde9922f0f94e048767",
+    "url": "/The-Look-Up/static/media/img_1.393751aa.jpg"
+  }
+]);
